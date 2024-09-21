7e59896a0f085767ed34df2665f2112ebbeab176
cnlohr <lohr85@gmail.com>
2024-09-21 02:01:13 -0700

    Update with format converter

---
+++ Assets/rv32im/TestApp/README.txt
@@ -0,0 +1,8 @@
+Pistol is "Nintendo blaster" by Alexander Brodin
+https://poly.pizza/m/6mgzhAFknsj
+
+Baked by:
+
+Blender, "Render" on vertical menu.
+Switch to "Cycles" Renderer
+Bake->Diffuse->Color->Active Color Attribute.
--- Assets/rv32im/TestApp/compile.bat
+++ Assets/rv32im/TestApp/compile.bat
@@ -1,3 +1,6 @@
+tcc objtoh.c -o objtoh.exe
+objtoh pistol.obj pistol.h pistol 4096 1
+
 riscv64-unknown-elf-gcc -T vrc-rv32im.lds  -Wl,--gc-sections -Wl,--print-memory-usage  -nostdlib -static-libgcc -ffunction-sections -fdata-sections -flto -march=rv32im -mabi=ilp32  test.S test.c -o test.elf
 riscv64-unknown-elf-objdump -S test.elf > test.lst
 riscv64-unknown-elf-objdump -t test.elf > test.map
